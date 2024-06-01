2edae1ac60f01d4cf9472a5e2719ca8802ae937b
Alina Garifjanova <alina.garifjanova@yh.nackademin.se>
2024-06-01 14:38:43 +0200

    Small update on program

--- Manero-UserProvider/Program.cs
+++ Manero-UserProvider/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Messaging.ServiceBus;
 using Manero_UserProvider.Handler;
 using Manero_UserProvider.Services;
 using Microsoft.Azure.Functions.Worker;
@@ -13,6 +14,16 @@ var host = new HostBuilder()
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
         services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
+
+        string serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnection");
+        services.AddSingleton<ServiceBusClient>(sp => new ServiceBusClient(serviceBusConnectionString));
+
+        services.AddSingleton<ServiceBusSender>(sp =>
+        {
+            var client = sp.GetRequiredService<ServiceBusClient>();
+            return client.CreateSender("account-request"); 
+        });
+
         services.AddSingleton<ServiceBusHandler>();
         services.AddScoped<ServiceBusHandler>();
         services.AddScoped<CreateService>();
